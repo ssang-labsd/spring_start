73e030ea294267d379dc971e410b00bb87e5971f
SSANG <ssang@labsd.net>
2024-07-10 15:24:30 +0900

    :bulb: Counting 부분 함수 주석처리
    - DI를 이용한 counting 설명을 위한 임시코드를 주석처리함

--- src/main/java/com/example/spring_start/user/dao/CountingDaoFactory.java
+++ src/main/java/com/example/spring_start/user/dao/CountingDaoFactory.java
@@ -1,22 +1,22 @@
-package com.example.spring_start.user.dao;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CountingDaoFactory {
-    @Bean
-    public UserDao userDao() {
-        return new UserDao(connectionMaker());
-    }
-
-    @Bean
-    public ConnectionMaker connectionMaker() {
-        return new CountingConnectionMaker(realConnectionMaker());
-    }
-
-    @Bean
-    public ConnectionMaker realConnectionMaker(){
-        return new DConnectionMaker();
-    }
-}
+//package com.example.spring_start.user.dao;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class CountingDaoFactory {
+//    @Bean
+//    public UserDao userDao() {
+//        return new UserDao();
+//    }
+//
+//    @Bean
+//    public ConnectionMaker connectionMaker() {
+//        return new CountingConnectionMaker(realConnectionMaker());
+//    }
+//
+//    @Bean
+//    public ConnectionMaker realConnectionMaker(){
+//        return new DConnectionMaker();
+//    }
+//}
