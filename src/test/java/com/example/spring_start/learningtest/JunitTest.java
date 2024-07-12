4a79a6e96441427ec231db7d02c30999ccae9dbd
SSANG <ssang@labsd.net>
2024-07-12 14:05:13 +0900

    :white_check_mark: 순서 상관없이 모든 JUnitTEst 객체 비교하게 테스트 코드 수정

--- src/test/java/com/example/spring_start/learningtest/JunitTest.java
+++ src/test/java/com/example/spring_start/learningtest/JunitTest.java
@@ -2,25 +2,29 @@ package com.example.spring_start.learningtest;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItem;
 
 public class JunitTest {
-    static JunitTest testObject;
+    static Set<JunitTest> testObjects = new HashSet<JunitTest>();
 
     @Test public void test1(){
-        assert(this != testObject);
-        testObject = this;
+        assert(!testObjects.contains(this));
+        testObjects.add(this);
     }
 
     @Test public void test2(){
-        assert(this != testObject);
-        testObject = this;
+        assert(!testObjects.contains(this));
+        testObjects.add(this);
     }
 
     @Test public void test3(){
-        assert(this != testObject);
-        testObject = this;
+        assert(!testObjects.contains(this));
+        testObjects.add(this);
     }
 }
