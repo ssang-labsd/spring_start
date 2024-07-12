796bb4bb38f6da77925cece0990c96d08a11724c
SSANG <ssang@labsd.net>
2024-07-12 14:34:32 +0900

    :white_check_mark: Spring 컨텍스트 기능에 대한 학습 테스트 진행

--- src/test/java/com/example/spring_start/learningtest/JunitTest.java
+++ src/test/java/com/example/spring_start/learningtest/JunitTest.java
@@ -1,6 +1,11 @@
 package com.example.spring_start.learningtest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,21 +15,36 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasItem;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 public class JunitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JunitTest> testObjects = new HashSet<JunitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test public void test1(){
         assert(!testObjects.contains(this));
         testObjects.add(this);
+
+        assert(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test public void test2(){
         assert(!testObjects.contains(this));
         testObjects.add(this);
+
+        assert(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test public void test3(){
         assert(!testObjects.contains(this));
         testObjects.add(this);
+
+        assert(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 }
