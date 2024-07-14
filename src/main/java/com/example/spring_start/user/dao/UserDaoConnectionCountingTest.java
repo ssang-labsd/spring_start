aff00e84c65a717e9ff7d94c607889f1cc4b2a0e
SeongHun_Cho <jng00153@gmail.com>
2024-07-15 01:30:46 +0900

    Revert ":recycle: 새로운 형태로 바꾼 클래스들 및 사용하지 않는 코드 제거"
    This reverts commit c0cc4d3561db5d6a62f0040681f4b13ef4a9efdc.

---
+++ src/main/java/com/example/spring_start/user/dao/UserDaoConnectionCountingTest.java
@@ -0,0 +1,21 @@
+//package com.example.spring_start.user.dao;
+//
+//import com.example.spring_start.SpringStartApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//import java.sql.SQLException;
+//
+//public class UserDaoConnectionCountingTest {
+//    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//        SpringApplication.run(SpringStartApplication.class, args);
+//
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//
+//        //Dao 사용 코드
+//
+//        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+//        System.out.println("Connection counter : " + ccm.getCounter());
+//    }
+//}
