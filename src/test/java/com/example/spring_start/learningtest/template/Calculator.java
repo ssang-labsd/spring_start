0d3a6eaf83cfa3ffb060cf61e32c79e59c003665
SSANG <ssang@labsd.net>
2024-07-15 16:03:22 +0900

    :recycle: try/catch/finally 구문으로 예외처리 추가 리팩토링

--- src/test/java/com/example/spring_start/learningtest/template/Calculator.java
+++ src/test/java/com/example/spring_start/learningtest/template/Calculator.java
@@ -6,14 +6,24 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(filepath));
-        Integer sum = 0;
-        String line = null;
-        while ((line = br.readLine()) != null) {
-            sum += Integer.valueOf(line);
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            while((line = br.readLine()) != null){
+                sum += Integer.parseInt(line);
+            }
+            return sum;
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally{
+            if(br != null){
+                try { br.close();}
+                catch(IOException e) { System.out.println(e.getMessage()); }
+            }
         }
-
-        br.close();
-        return sum;
     }
+
 }
