c5585a8bcd52bc26952ee5f9494672f29308e8af
SSANG <ssang@labsd.net>
2024-07-15 16:38:52 +0900

    :recycle: 라인 읽는 중복 부분 메소드 추출하여 중복 제거

--- src/test/java/com/example/spring_start/learningtest/template/Calculator.java
+++ src/test/java/com/example/spring_start/learningtest/template/Calculator.java
@@ -6,50 +6,37 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback =
-                new BufferedReaderCallback() {
-                    @Override
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer sum = 0;
-                        String line = null;
-                        while((line = br.readLine()) != null) {
-                            sum += Integer.valueOf(line);
-                        }
-                        return sum;
-                    }
-        };
-        return fileReadTemplate(filepath, sumCallback);
+        LineCallback sumCallback = new LineCallback() {
+            public Integer doSomethingWithLine(String line, Integer value){
+                return value + Integer.valueOf(line);
+            }};
+        return lineReadTemplate(filepath,sumCallback,0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallback multiplyCallback =
-                new BufferedReaderCallback() {
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer multiply = 1;
-                        String line = null;
-                        while((line = br.readLine()) != null) {
-                            multiply *= Integer.valueOf(line);
-                        }
-                        return multiply;
-                    }
-                };
-        return fileReadTemplate(filepath, multiplyCallback);
+        LineCallback multiplyCallback = new LineCallback() {
+            public Integer doSomethingWithLine(String line, Integer value){
+                return value * Integer.valueOf(line);
+            }};
+        return lineReadTemplate(filepath,multiplyCallback,1);
     }
 
-    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
         BufferedReader br = null;
         try{
             br = new BufferedReader(new FileReader(filepath));
-            int ret = callback.doSomethingWithReader(br);
-            return ret;
-        } catch(IOException e) {
+            Integer res = initVal;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
+        } catch(IOException e){
             System.out.println(e.getMessage());
             throw e;
-        }
-        finally{
+        } finally {
             if(br != null){
-                try { br.close();
-                } catch(IOException e) { System.out.println(e.getMessage()); }
+                try { br.close(); } catch(IOException e) { System.out.println(e.getMessage());}
             }
         }
     }
---
+++ src/test/java/com/example/spring_start/learningtest/template/LineCallback.java
@@ -0,0 +1,5 @@
+package com.example.spring_start.learningtest.template;
+
+public interface LineCallback {
+    Integer doSomethingWithLine(String line, Integer value);
+}
