7e85fceed24bab92243d54e09327a2f91c9c58bb
SSANG <ssang@labsd.net>
2024-07-17 15:17:13 +0900

    :recycle: TransactionManager 적용하여 리팩토링

--- src/main/java/com/example/spring_start/user/service/UserService.java
+++ src/main/java/com/example/spring_start/user/service/UserService.java
@@ -3,7 +3,11 @@ package com.example.spring_start.user.service;
 import com.example.spring_start.user.dao.UserDao;
 import com.example.spring_start.user.domain.Level;
 import com.example.spring_start.user.domain.User;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.sql.DataSource;
@@ -25,9 +29,10 @@ public class UserService {
     }
 
     public void upgradeLevels() throws Exception{
-        TransactionSynchronizationManager.initSynchronization();
-        Connection c = DataSourceUtils.getConnection(dataSource);
-        c.setAutoCommit(false);
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
         try{
             List<User> users = userDao.getAll();
             for(User user : users){
@@ -35,16 +40,11 @@ public class UserService {
                     upgradeLevel(user);
                 }
             }
-            c.commit();
+            transactionManager.commit(status);
         } catch(Exception e){
-            c.rollback();
+            transactionManager.rollback(status);
             throw e;
-        } finally{
-            DataSourceUtils.releaseConnection(c, dataSource);
-            TransactionSynchronizationManager.unbindResource(this.dataSource);
-            TransactionSynchronizationManager.clearSynchronization();
         }
-
     }
 
     private boolean canUpgradeLevel(User user){
